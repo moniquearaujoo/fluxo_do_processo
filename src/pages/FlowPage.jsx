df9db51365a8345f0955a5f89acc24f3210383bf
Gaabs0 <acarlosgabriel92@gmail.com>
2025-05-31 17:20:57 -0300

    cliclo2

--- src/pages/FlowPage.jsx
+++ src/pages/FlowPage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import About from "../components/About";
+
+
 import FlowHome from "../components/FlowHome";
 
 function FlowPage() {
   return (
     <div className="flowpage-container">
       <Navbar />
-      <About />
+    
       <FlowHome />
     </div>
   );
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "../components/Header";
-import About from "../components/About";
 import Team from "../components/Team";
 import Navbar from "../components/Navbar";
 
@@ -8,8 +7,7 @@ function LandingPage() {
   return (
     <div className="landing-container">
       <Navbar />
-      <Header />
-      <About />       
+      <Header />  
       <Team />
     </div>
   );
